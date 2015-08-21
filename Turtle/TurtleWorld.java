bd9421757a5fad8f5b68c341fb0bf874b4d0aec4
Alex Anderson <thesuperuser2.0@outlook.com>
2015-08-21 10:13:49 -0500

    Updated contents from previous edits
    Hello, world!

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,32 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
         turtle.penDown();
-        turtle.forward(50);
+        for(int z=0; z<50; z++){
+            for(int x=0; x<14; x++){
+                for(int y=0; y<7; y++){
+                    turtle.setPenColor(Color.GREEN);
+                    turtle.forward(14*x+14+y);
+                    turtle.turnRight();
+                    turtle.setPenColor(Color.BLUE);
+                    turtle.forward(14*x+14+y);
+                    turtle.turnRight();
+                    turtle.setPenColor(Color.RED);
+                    turtle.forward(14*x+14+y);
+                    turtle.turnRight();
+                    turtle.setPenColor(Color.YELLOW);
+                    turtle.forward(14*x+14+y);
+                    turtle.turnRight();
+                    turtle.setPenColor(Color.BLACK);
+                    turtle.forward(14*x+14+y);
+                    turtle.turnRight();
+                }
+            }
+        }
+        turtle.penUp();
+        turtle.turnRight();
+        turtle.turnRight();
+        turtle.forward(200);
+        turtle.penDown();
     }
 }
